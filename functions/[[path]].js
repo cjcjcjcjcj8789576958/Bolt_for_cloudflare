cabc9424a2699909736a0a76d7809581155b9445
flipyak <109098370+cjcjcjcjcj8789576958@users.noreply.github.com>
2025-11-07 14:47:51 -0500

    Set up Cloudflare handler for Express server

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,1 +1,10 @@
 
+// functions/[[path]].js
+import app from '../index.js'; // Assumes the main Express app is exported from index.js
+import { httpServerHandler } from 'cloudflare:node';
+
+// Bolt runs on a specific port. We use that port to listen internally.
+const server = app.listen(8080);
+
+// Export the handler to bridge the incoming Cloudflare request to the Express server
+export default httpServerHandler(server);
